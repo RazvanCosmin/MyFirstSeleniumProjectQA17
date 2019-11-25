c29d6ae19481c6a79c5999d71b5184cfe89dc22f
Cosmin Razvan <razvancluj88@yahoo.com>
2019-11-25 18:16:02 +0200

    u

--- src/test/java/LoginText.java
+++ src/test/java/LoginText.java
@@ -30,6 +30,15 @@ public class LoginText {
         loginButton.click();
         WebElement welcomeMessageElement = driver.findElement(By.cssSelector("#maincontent > div.columns > div.column.main > div.block.block-dashboard-info > div.block-title > strong"));
         Assert.assertEquals(welcomeMessageElement.getText(), "Contul meu");
+        driver.findElement(By.cssSelector("#search"));
+        driver.findElement(By.cssSelector("#shopping-cart-table > tbody > tr.item-actions > td > div > a.action.action-delete > span"));
+        driver.findElement(By.cssSelector("#sorter"));
+        driver.findElement(By.cssSelector("body"));
+        driver.findElement(By.cssSelector("#maincontent > div.columns > div.column.main > div:nth-child(3) > h2"));
+        driver.findElement(By.cssSelector("body > div.page-wrapper > div.footer-container.page-footer > div > div > div.footer-primary-container.section-container > div > div > div.clearfix > div:nth-child(3) > div > div > div > div:nth-child(4) > p > a"));
+        driver.findElement(By.cssSelector("body"));
+        driver.findElement(By.cssSelector("#top > div > div.header-top-container > div > div > div.left-column > div:nth-child(1) > a:nth-child(2)"));
+
     }
         @After
          public void closeDriver(){
